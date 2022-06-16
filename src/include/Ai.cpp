f330ccc42958683c9234221089ab04dcdd363a56
Jonathan Stancik <jonathan.e.stancik@gmail.com>
2022-06-15 21:59:03 -0400

    added dependancies

--- src/include/Ai.cpp
+++ src/include/Ai.cpp
@@ -5,6 +5,31 @@
 #include <iostream>
 
 #include "Ai.hpp"
+void Brain::operator=(const Brain & brain0){
+    weights.resize(brain0.weights.size());
+
+    for(int i=0;i<weights.size();i++){
+        weights[i].resize(brain0.weights[i].size());
+        for(int j=0;j<weights[i].size();j++){
+            weights[i][j].resize(brain0.weights[i][j].size());
+            for(int k=0;k<weights[i][j].size();k++){
+                    weights[i][j][k] = brain0.weights[i][j][k];
+            }
+        }
+    }
+
+    bias.resize(brain0.bias.size());
+    for(int i=0;i<bias.size();i++){
+        bias[i].resize(brain0.bias[i].size());
+        for(int j=0;j<bias[i].size();j++){
+                bias[i][j] = brain0.bias[i][j];
+        }
+    }
+    nodes.resize(brain0.nodes.size());
+    for(int i=0;i<nodes.size();i++){
+        nodes[i].resize(brain0.nodes[i].size());
+    }
+}
 
 Brain::Brain(int input, int output){
     int layerCount = 3+(rand()*3/RAND_MAX);
